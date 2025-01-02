0f501586ae3dba8f7e5e3724bf6ea8c163f11cfd
kengo2003 <saitokengo2003@gmail.com>
2025-01-03 01:54:05 +0900

    dev

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -19,15 +19,20 @@ enum WebEvent {
 }
 struct Car {
     color: String,
-    transmission: Transmission,
-    convertible: bool,
-    mileage: u32,
+    motor: Transmission,
+    roof: bool,
+    age: (Age, u32),
 }
 #[derive(PartialEq, Debug)]
 enum Transmission {
     Manual,
     SemiAuto,
     Automatic,
+}
+#[derive(PartialEq, Debug)]
+enum Age {
+    New,
+    Used,
 }
 fn main() {
     println!("Hello, world!");
@@ -81,35 +86,60 @@ fn main() {
     );
     goodbye("Goodbye");
 
-    let mut car = car_factory(String::from("Red"), Transmission::Manual, false);
+    // let mut car = car_factory(String::from("Red"), Transmission::Manual, false);
+    // println!(
+    //     "Car 1 = {}, {:?} transmission, convertible: {}, mileage: {}",
+    //     car.color, car.transmission, car.convertible, car.mileage
+    // );
+    // car = car_factory(String::from("Silver"), Transmission::Automatic, true);
+    // println!(
+    //     "Car 2 = {}, {:?} transmission, convertible: {}, mileage: {}",
+    //     car.color, car.transmission, car.convertible, car.mileage
+    // );
+    // car = car_factory(String::from("Yellow"), Transmission::SemiAuto, true);
+    // println!(
+    //     "Car 3 = {}, {:?} transmission, convertible: {}, mileage: {}",
+    //     car.color, car.transmission, car.convertible, car.mileage
+    // );
+    vec();
+
+    let colors = ["Blue", "Green", "Red", "Silver"];
+    let mut car: Car;
+    let mut engine = Transmission::Manual;
+
+    car = car_factory(String::from(colors[0]), engine, true, 0);
     println!(
-        "Car 1 = {}, {:?} transmission, convertible: {}, mileage: {}",
-        car.color, car.transmission, car.convertible, car.mileage
-    );
-    car = car_factory(String::from("Silver"), Transmission::Automatic, true);
-    println!(
-        "Car 2 = {}, {:?} transmission, convertible: {}, mileage: {}",
-        car.color, car.transmission, car.convertible, car.mileage
-    );
-    car = car_factory(String::from("Yellow"), Transmission::SemiAuto, true);
-    println!(
-        "Car 3 = {}, {:?} transmission, convertible: {}, mileage: {}",
-        car.color, car.transmission, car.convertible, car.mileage
+        "Car order 1: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        car.age.0, car.roof, car.motor, car.color, car.age.1
     );
 
-    vec();
+    // Car order #2: Used, Semi-automatic, Convertible
+    engine = Transmission::SemiAuto;
+    car = car_factory(String::from(colors[1]), engine, false, 100);
+    println!(
+        "Car order 2: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
+
+    // Car order #3: Used, Automatic, Hard top
+    engine = Transmission::Automatic;
+    car = car_factory(String::from(colors[2]), engine, true, 200);
+    println!(
+        "Car order 3: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
 }
 
 fn goodbye(text: &str) {
     println!("text: {}", text)
 }
 
-fn car_factory(color: String, transmission: Transmission, convertible: bool) -> Car {
+fn car_factory(color: String, motor: Transmission, roof: bool, miles: u32) -> Car {
     Car {
         color: color,
-        transmission: transmission,
-        convertible: convertible,
-        mileage: 0,
+        motor: motor,
+        roof: roof,
+        age: car_quality(miles),
     }
 }
 
@@ -126,4 +156,9 @@ fn vec() {
     println!("Fruits: {:?}", fruit);
     fruit.pop();
     println!("Fruits: {:?}", fruit)
+}
+
+fn car_quality(miles: u32) -> (Age, u32) {
+    let quality = (Age::New, miles);
+    quality
 }
